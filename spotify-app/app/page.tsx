954516ec4a6d95287d475e75aeba8466e7632ee0
David Motorca <dmotorca@pdx.edu>
2024-03-20 13:14:22 -0700

    Started using nextjs14 , created host/join for navigation. Ready to begin spotifyAuth

---
+++ spotify-app/app/page.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-center text-spotify text-4xl">Spotify Voting</h1>
+      <div className="flex flex-row justify-center mb-72">
+        <Link href="/host" className="text-center p-4 mx-4">
+          HOST
+        </Link>
+        <Link href="/join" className="text-center p-4 mx-4">
+          JOIN
+        </Link>
+      </div>
+    </main>
+  );
+}
